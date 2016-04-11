dc7f5766e8d821e48d57dbdefb58cd9e57dd8a93
Andrew Templeton <andrw.tmplton@gmail.com>
2016-04-10 22:29:46 -0500

    clean out the debug function

--- src/scripts/debug.js
+++ src/scripts/debug.js
@@ -1,19 +1,3 @@
 module.exports = function (app) {
-	var deviceProps = [
-		'active',
-		'available',
-		'id',
-		'index',
-		'name',
-		'kind',
-		'networkAddress',
-		'persistentID',
-		'protected',
-		'selected',
-		'supportsAudio',
-		'supportsVideo',
-		'soundVolume'
-	];
-	var bedroom = app.airplayDevices()[1];
-	app.currentAirPlayDevices = bedroom;
+	
 };
